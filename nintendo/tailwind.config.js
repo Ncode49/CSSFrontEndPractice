696662c74e2223c644ff45b78021bc3cf5f69ea2
Nicky Tching <nicky.tching@grenoble-inp.org>
2022-02-10 16:05:22 +0100

    correct big size media query

--- nintendo/tailwind.config.js
+++ nintendo/tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    
-    extend: {},
+    screens: {
+      sm: "576px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "960px",
+      // => @media (min-width: 960px) { ... }
+
+      lg: "1440px",
+      // => @media (min-width: 1440px) { ... }
+      desktop: "1115px",
+    },
   },
   plugins: [],
 };
